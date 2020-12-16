83994c90f84b79dfe3d695c4bf635dfe731d3fac
Bayashiko0226 <52346150+Bayashiko0226@users.noreply.github.com>
2020-12-17 01:15:36 +0900

    update dec.c

--- grpwk20/ADSB2-19/dec.c
+++ grpwk20/ADSB2-19/dec.c
@@ -5,21 +5,23 @@
 #pragma GCC optimize("Ofast")
 #define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
 #define max(a,b) a>b?a:b
+#define N 9
+#define M 18
 
-int edis(unsigned char *S,int n,unsigned char *T){
-  int dp[n+1][n+1];
-  rep(i,n+1) dp[i][0]=i;
-  rep(i,n) dp[0][i+1]=i+1;
-  for(int i=1;i<=n;i++) for(int j=1;j<=n;j++){
+unsigned char edis(unsigned char *S,unsigned char *T){
+  unsigned char dp[M+1][M+1];
+  rep(i,M+1) dp[i][0]=i;
+  rep(i,M) dp[0][i+1]=i+1;
+  for(int i=1;i<=M;i++) for(int j=1;j<=M;j++){
     int min;
-    int add=S[i-1]==T[i-1]?0:1;
+    //int add=S[i-1]==T[i-1]?0:1;
     //if(dp[i-1][j-1]+add<min) min=dp[i-1][j-1]+add;
-    min=dp[i-1][j-1]+add;
+    min=dp[i-1][j-1]+(S[i-1]==T[i-1]?0:1);
     if(dp[i-1][j]+1<min) min=dp[i-1][j]+1;
     if(dp[i][j-1]+1<min) min=dp[i][j-1]+1;
     dp[i][j]=min;
   }
-  return dp[n][n];
+  return dp[M][M];
 }
 
 int dec(){
@@ -37,7 +39,6 @@ int dec(){
 
   unsigned char c, res;
   int cur;
-  int N=9,M=15;
   unsigned char S[N][205000];
   rep(i,N){
     cur=0;
@@ -64,8 +65,6 @@ int dec(){
     }
     for(int j=cur;j<205000;j++) S[i][j]=4;
   }
-  //0,1 > 0 
-  //2,3 > 1
   int pt[N];
   rep(i,N) pt[i]=0;
   unsigned char now[N];
@@ -86,25 +85,23 @@ int dec(){
     if(zero>=one) fputc('0',dfp),wrong=1;
     else fputc('1',dfp),wrong=0;
     rep(j,M+3){
-      int chose=0;
-      int n[4]={};
+      int n[5]={};
       rep(k,N){
-        if(S[k][pt[k]+j]==4) continue;
+        if(S[k][pt[k]+j]==4) n[4]++;
         else if(wrong){
-          if(now[k]>=2) continue; 
-          chose++;
+          if(now[k]>=2) continue;
+          if(((S[k][pt[k]+j])&1)^((i+j)&1)) continue;
           n[S[k][pt[k]+j]]++;
         }
         else{
           if(now[k]<2) continue;
-          chose++;
+          if(((S[k][pt[k]+j])&1)^((i+j)&1)) continue;
           n[S[k][pt[k]+j]]++;
         }
       }
       int plc=0,maxi=0;
-      rep(k,4) if(n[k]>maxi) plc=k,maxi=n[k];
-      if(chose==0) base[j]=4;
-      else base[j]=plc;
+      rep(k,5) if(n[k]>maxi) plc=k,maxi=n[k];
+      base[j]=plc;
     }
     unsigned char base1[M],base2[M],base3[M],base4[M];
     rep(j,M) base1[j]=base[j],base2[j]=base[j+1],base3[j]=base[j+2],base4[j]=base[j+3];
@@ -117,13 +114,13 @@ int dec(){
         for(int k=pt[j]+2;k<=pt[j]+M+1;k++) cmp2[k-pt[j]-2]=S[j][k];
         for(int k=pt[j]+3;k<=pt[j]+M+2;k++) cmp3[k-pt[j]-3]=S[j][k];
         for(int k=pt[j];k<=pt[j]+M-1;k++) cmp4[k-pt[j]]=S[j][k];
-        int diff[6];
-        diff[0]=edis(base1,M,cmp1);
-        diff[1]=edis(base1,M,cmp2);
-        diff[2]=edis(base1,M,cmp3);
-        diff[3]=edis(base2,M,cmp4);
-        diff[4]=edis(base3,M,cmp4);
-        diff[5]=edis(base4,M,cmp4);
+        unsigned char diff[6];
+        diff[0]=edis(base1,cmp1);
+        diff[1]=edis(base1,cmp2);
+        diff[2]=edis(base1,cmp3);
+        diff[3]=edis(base2,cmp4);
+        diff[4]=edis(base3,cmp4);
+        diff[5]=edis(base4,cmp4);
         int mini=2*M,use=0;
         rep(k,6) if(mini>diff[k]) mini=diff[k],use=k;
         if(use==0) pt[j]++;
@@ -140,13 +137,13 @@ int dec(){
         for(int k=pt[j]+2;k<=pt[j]+M+1;k++) cmp2[k-pt[j]-2]=S[j][k];
         for(int k=pt[j]+3;k<=pt[j]+M+2;k++) cmp3[k-pt[j]-3]=S[j][k];
         for(int k=pt[j];k<=pt[j]+M-1;k++) cmp4[k-pt[j]]=S[j][k];
-        int diff[6];
-        diff[0]=edis(base1,M,cmp1);
-        diff[1]=edis(base1,M,cmp2);
-        diff[2]=edis(base1,M,cmp3);
-        diff[3]=edis(base2,M,cmp4);
-        diff[4]=edis(base3,M,cmp4);
-        diff[5]=edis(base4,M,cmp4);
+        unsigned char diff[6];
+        diff[0]=edis(base1,cmp1);
+        diff[1]=edis(base1,cmp2);
+        diff[2]=edis(base1,cmp3);
+        diff[3]=edis(base2,cmp4);
+        diff[4]=edis(base3,cmp4);
+        diff[5]=edis(base4,cmp4);
         int mini=2*M,use=0;
         rep(k,6) if(mini>diff[k]) mini=diff[k],use=k;
         if(use==0) pt[j]++;
@@ -162,6 +159,9 @@ int dec(){
       if(pt[j]>=204900) pt[j]=201000;
     } 
   }
+  /*rep(i,N){
+    printf("%d ",pt[i]);
+  }*/
   res = '\n';
   fputc(res, dfp); 
   fclose(sfp);
