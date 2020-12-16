7f651a6cf65bae0a5797dfb01129af2f8da470ef
Bayashiko0226 <yuki2001_grimoire@mineo.jp>
2020-12-16 12:15:08 +0900

    fix solution for NP method

--- grpwk20/ADSB2-19/dec.c
+++ grpwk20/ADSB2-19/dec.c
@@ -6,20 +6,20 @@
 #define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
 #define max(a,b) a>b?a:b
 
-int edis(unsigned char *S,int n,unsigned char *T,int m){
-  int dp[n+1][m+1];
-  rep(i,n+1) rep(j,m+1) dp[i][j]=0;
+int edis(unsigned char *S,int n,unsigned char *T){
+  int dp[n+1][n+1];
   rep(i,n+1) dp[i][0]=i;
-  rep(i,m+1) dp[0][i]=i;
-  for(int i=1;i<=n;i++) for(int j=1;j<=m;j++){
-    int min=1e9;
+  rep(i,n) dp[0][i+1]=i+1;
+  for(int i=1;i<=n;i++) for(int j=1;j<=n;j++){
+    int min;
     int add=S[i-1]==T[i-1]?0:1;
-    if(dp[i-1][j-1]+add<min) min=dp[i-1][j-1]+add;
+    //if(dp[i-1][j-1]+add<min) min=dp[i-1][j-1]+add;
+    min=dp[i-1][j-1]+add;
     if(dp[i-1][j]+1<min) min=dp[i-1][j]+1;
     if(dp[i][j-1]+1<min) min=dp[i][j-1]+1;
     dp[i][j]=min;
   }
-  return dp[n][m];
+  return dp[n][n];
 }
 
 int dec(){
@@ -37,8 +37,8 @@ int dec(){
 
   unsigned char c, res;
   int cur;
-  int N=20;
-  unsigned char S[N][210000];
+  int N=9,M=15;
+  unsigned char S[N][205000];
   rep(i,N){
     cur=0;
     while((c = getc(sfp)) != '\n'){
@@ -62,7 +62,7 @@ int dec(){
       S[i][cur]=res;
       cur++; 
     }
-    for(int j=cur;j<210000;j++) S[i][j]=4;
+    for(int j=cur;j<205000;j++) S[i][j]=4;
   }
   //0,1 > 0 
   //2,3 > 1
@@ -70,9 +70,10 @@ int dec(){
   rep(i,N) pt[i]=0;
   unsigned char now[N];
   int zero,one,wrong;
+  unsigned char base[M+3];
   rep(i,200000){
     rep(j,N) now[j]=S[j][pt[j]];
-    /*if(i<=100){
+    /*if(i>=199900){
       rep(j,N) printf("%d ",now[j]);
       printf("\n");
     }*/
@@ -84,8 +85,7 @@ int dec(){
     }
     if(zero>=one) fputc('0',dfp),wrong=1;
     else fputc('1',dfp),wrong=0;
-    unsigned char base[33];
-    rep(j,33){
+    rep(j,M+3){
       int chose=0;
       int n[4]={};
       rep(k,N){
@@ -106,25 +106,25 @@ int dec(){
       if(chose==0) base[j]=4;
       else base[j]=plc;
     }
-    unsigned char base1[30],base2[30],base3[30],base4[30];
-    rep(j,30) base1[j]=base[j],base2[j]=base[j+1],base3[j]=base[j+2],base4[j]=base[j+3];
+    unsigned char base1[M],base2[M],base3[M],base4[M];
+    rep(j,M) base1[j]=base[j],base2[j]=base[j+1],base3[j]=base[j+2],base4[j]=base[j+3];
     rep(j,N){
       if(now[j]==4) continue;
       if(wrong){
         if(now[j]<2) continue;
-        unsigned char cmp1[30],cmp2[30],cmp3[30],cmp4[30];
-        for(int k=pt[j]+1;k<=pt[j]+30;k++) cmp1[k-pt[j]-1]=S[j][k];
-        for(int k=pt[j]+2;k<=pt[j]+31;k++) cmp2[k-pt[j]-2]=S[j][k];
-        for(int k=pt[j]+3;k<=pt[j]+32;k++) cmp3[k-pt[j]-3]=S[j][k];
-        for(int k=pt[j];k<=pt[j]+29;k++) cmp4[k-pt[j]]=S[j][k];
+        unsigned char cmp1[M],cmp2[M],cmp3[M],cmp4[M];
+        for(int k=pt[j]+1;k<=pt[j]+M;k++) cmp1[k-pt[j]-1]=S[j][k];
+        for(int k=pt[j]+2;k<=pt[j]+M+1;k++) cmp2[k-pt[j]-2]=S[j][k];
+        for(int k=pt[j]+3;k<=pt[j]+M+2;k++) cmp3[k-pt[j]-3]=S[j][k];
+        for(int k=pt[j];k<=pt[j]+M-1;k++) cmp4[k-pt[j]]=S[j][k];
         int diff[6];
-        diff[0]=edis(base1,30,cmp1,30);
-        diff[1]=edis(base1,30,cmp2,30);
-        diff[2]=edis(base1,30,cmp3,30);
-        diff[3]=edis(base2,30,cmp4,30);
-        diff[4]=edis(base3,30,cmp4,30);
-        diff[5]=edis(base4,30,cmp4,30);
-        int mini=60,use=0;
+        diff[0]=edis(base1,M,cmp1);
+        diff[1]=edis(base1,M,cmp2);
+        diff[2]=edis(base1,M,cmp3);
+        diff[3]=edis(base2,M,cmp4);
+        diff[4]=edis(base3,M,cmp4);
+        diff[5]=edis(base4,M,cmp4);
+        int mini=2*M,use=0;
         rep(k,6) if(mini>diff[k]) mini=diff[k],use=k;
         if(use==0) pt[j]++;
         else if(use==3) pt[j]--;
@@ -135,19 +135,19 @@ int dec(){
       }
       else{
         if(now[j]>=2) continue;
-        unsigned char cmp1[30],cmp2[30],cmp3[30],cmp4[30];
-        for(int k=pt[j]+1;k<=pt[j]+30;k++) cmp1[k-pt[j]-1]=S[j][k];
-        for(int k=pt[j]+2;k<=pt[j]+31;k++) cmp2[k-pt[j]-2]=S[j][k];
-        for(int k=pt[j]+3;k<=pt[j]+32;k++) cmp3[k-pt[j]-3]=S[j][k];
-        for(int k=pt[j];k<=pt[j]+29;k++) cmp4[k-pt[j]]=S[j][k];
+        unsigned char cmp1[M],cmp2[M],cmp3[M],cmp4[M];
+        for(int k=pt[j]+1;k<=pt[j]+M;k++) cmp1[k-pt[j]-1]=S[j][k];
+        for(int k=pt[j]+2;k<=pt[j]+M+1;k++) cmp2[k-pt[j]-2]=S[j][k];
+        for(int k=pt[j]+3;k<=pt[j]+M+2;k++) cmp3[k-pt[j]-3]=S[j][k];
+        for(int k=pt[j];k<=pt[j]+M-1;k++) cmp4[k-pt[j]]=S[j][k];
         int diff[6];
-        diff[0]=edis(base1,30,cmp1,30);
-        diff[1]=edis(base1,30,cmp2,30);
-        diff[2]=edis(base1,30,cmp3,30);
-        diff[3]=edis(base2,30,cmp4,30);
-        diff[4]=edis(base3,30,cmp4,30);
-        diff[5]=edis(base4,30,cmp4,30);
-        int mini=60,use=0;
+        diff[0]=edis(base1,M,cmp1);
+        diff[1]=edis(base1,M,cmp2);
+        diff[2]=edis(base1,M,cmp3);
+        diff[3]=edis(base2,M,cmp4);
+        diff[4]=edis(base3,M,cmp4);
+        diff[5]=edis(base4,M,cmp4);
+        int mini=2*M,use=0;
         rep(k,6) if(mini>diff[k]) mini=diff[k],use=k;
         if(use==0) pt[j]++;
         else if(use==3) pt[j]--;
@@ -157,9 +157,11 @@ int dec(){
         else pt[j]-=3;
       }
     }
-    rep(j,N) pt[j]++;
+    rep(j,N){
+      pt[j]++;
+      if(pt[j]>=204900) pt[j]=201000;
+    } 
   }
-  //rep(k,N) printf("%d ",pt[k]);
   res = '\n';
   fputc(res, dfp); 
   fclose(sfp);
