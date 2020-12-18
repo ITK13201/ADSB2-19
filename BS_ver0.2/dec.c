3d12c9264910b9b8ca9c95de93e771310d51c625
KOHsEMP <67769436+KOHsEMP@users.noreply.github.com>
2020-12-18 09:58:36 +0900

    Update dec.c
    ver0.2.2

--- BS_ver0.2/dec.c
+++ BS_ver0.2/dec.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "grpwk20.h"
-#define BLOCK_LEN 5556
-#define DATA_LEN 200000
-#define ITERATE 3 // 読み取る行数
-#define JUDGE 8   // 同一の箇所を読み取ったデータであると判定するハミング距離の閾値
-#define OVER 100  // 1つのindexに重複できるデータの個数
-#define WEIGHT 5  // 多数決決定法において当たりと判定されたものに付ける重み
+一の箇所を読み取ったデータであると判定するハミング距離の閾値
+#define OVER  1000 // 1つのindexに重複できるデータの個数
+#define OVER_sub 1000 // check関数用
+#define WEIGHT 10  // 多数決決定法において当たりと判定されたものに付ける重み
 
 typedef struct{
   int count;
+  int dist_plus;
   int flg[OVER]; // そのcount値が当たりであれば1、はずれであれば0
   unsigned char data[OVER][18];  
 }BLOCK;
@@ -17,7 +13,6 @@ typedef struct{
 // check関数用
 typedef struct{
   int left[ITERATE];
-  // int right[ITERATE]; // 最小となったときの相手のcount_subを入れる
   int ham[ITERATE];
   int flg[ITERATE];  // hamが8以下であれば1 そうで無ければ0
 }CHECK_SUB;
@@ -28,13 +23,13 @@ unsigned char id_char[7]; // distribute用
 int atari[BLOCK_LEN] = {0}; //指定のindexが当たりであれば1, はずれであれば0
 unsigned char mdres[20]; // 多数決結果を一時的に格納 majority decision result
 
-// FILE *sfp; // seqdata
 
 /*init_block: blockの初期化-------------------------------------------------------------------*/
 void init_block(){
   for(int i=0; i<ITERATE; i++){
     for(int j=0; j<BLOCK_LEN; j++){ 
       block[i][j].count = 0;
+      block[i][j].dist_plus = 0;
       // for(int k=0; k<ITERATE-1; k++)
       for(int k=0;k<OVER;k++){
         block[i][j].flg[k] = 0;
@@ -42,6 +37,7 @@ void init_block(){
       }
     }
   }
+  return;
 }
 
 /*init_check_sub: check_subの初期化-----------------------------------------------------------*/
@@ -53,12 +49,11 @@ void init_check_sub(){
       check_sub[i].ham[j] = 18;
       check_sub[i].flg[j] = 0;
     }
-    //for(int j=0; j<ITERATE-1; j++)
-      
   }
+  return;
 }
-/* hexa_sub: ATGCを数値に変換 (index用)-------------------------------------------------------*/
-int hexa_sub(unsigned char ATGC){
+/* quad_sub: ATGCを数値に変換 (index用)-------------------------------------------------------*/
+int quad_sub(unsigned char ATGC){
   switch(ATGC){
     case 'A': return 0;
     case 'C': return 1;
@@ -67,41 +62,25 @@ int hexa_sub(unsigned char ATGC){
   }
   return 0;
 }
-/* hexa2num: 4進数を10進数に変換 (index用)----------------------------------------------------*/
-int hexa2num(unsigned char hexa[]){
+/* quad2num: 4進数を10進数に変換 (index用)----------------------------------------------------*/
+int quad2num(unsigned char quad[]){
   int num=0;
   int tmp = 0;
-  int dis = 0;
+  //int dis = 0;
   int i = 4096;
   for(int j=0; j<7; j++){
-    tmp = hexa_sub(hexa[j]);
+    tmp = quad_sub(quad[j]);
     num += tmp * i;
     if(j!=6) i = i/4;
   }
   i = 1;
   if(num > 5555){ // 置換エラーで明らかにアウトなインデックスが出た時 GGCTGGA以上
-    /*if(hexa[0] == 'T'){
-      num -= 4096*2;
-    }else if(hexa[0]=='G'){
-      num -= 4096;
-    }else{
-      tmp = num % 5;
-      dis = 5555 - num;
-      for(int j=0; j<tmp; j++){
-        i = i*4;
-        if(i>dis){
-          i = i/4;
-          break;
-        }
-      }
-      num += i;
-    }*/
     num = rand() % 5555;
   }
   return num;
 }
 
-/* conv_sub: num2hexaの補助関数---------------------------------------------------------------*/
+/* conv_sub: num2quadの補助関数---------------------------------------------------------------*/
 unsigned char conv_sub(int a){
   switch(a){
     case 0: return 'A';
@@ -112,8 +91,8 @@ unsigned char conv_sub(int a){
   return 'A';
 }
 
-/*num2hexa 10進数をATGC4進数に変換------------------------------------------------------------*/
-void num2hexa(int num){
+/*num2quad 10進数をATGC4進数に変換------------------------------------------------------------*/
+void num2quad(int num){
   int i = 4096; // 4^6
   int tmp = num; // 各桁の値
   int j=0; 
@@ -136,17 +115,6 @@ unsigned char ATGC2c1(unsigned char c){
     case 'T': return '1';
   }
   return '0';
-  /*switch(c){
-    case 'A': res = '0';
-              break;
-    case 'C': res = '0';
-              break;
-    case 'G': res = '1';
-              break;
-    case 'T': res = '1';
-              break;
-  }
-  return res;*/
 }
 /* ATGC2c2: 文字cから右側を取り出す---------------------------------------------------------*/
 unsigned char ATGC2c2(unsigned char c){
@@ -158,17 +126,6 @@ unsigned char ATGC2c2(unsigned char c){
     case 'T': return '1';
   }
   return '0';
-  /*switch(c){
-    case 'A': res = '0';
-              break;
-    case 'C': res = '1';
-              break;
-    case 'G': res = '0';
-              break;
-    case 'T': res = '1';
-              break;
-  }
-  return res;*/
 }
 
 /* hamming: 二つのデータのハミング距離を計算 (文字が異なれば+1)-------------------------------*/
@@ -186,16 +143,17 @@ int check(int index){
   init_check_sub();
   int res =0;
   int search_cnt = 0;
-  int iter_sub[OVER*3] = {-1};
-  int count_sub[OVER*3] = {-1};
+  int tmp = 0;
+  int tmp_flg = 0;
+  int iter_sub[OVER_sub] = {-1};
+  int count_sub[OVER_sub] = {-1};
   for(int i=0; i<ITERATE; i++){
-    for(int j=0; j<block[ITERATE][index].count; j++){
+    for(int j=0; j<block[i][index].count; j++){
       iter_sub[search_cnt] = i; // iterateの番号を入れる
       count_sub[search_cnt] = j; // countの番号を入れる
       search_cnt++;
     }
   }
-  int tmp = 0;
   // indexにある要素のハミング距離を比較する
   for(int i=0; i<search_cnt; i++){
     for(int j=i+1; j<search_cnt; j++){
@@ -214,7 +172,6 @@ int check(int index){
       }
     }
   }
-  int tmp_flg = 0;
   // int flg_cnt = 0;
   for(int i=0; i<ITERATE; i++){
     tmp_flg = 0;
@@ -227,9 +184,7 @@ int check(int index){
     if(tmp_flg == 1){ // 今見ているITERATEに当たりが1つでもあった場合
       for(int j=0; j<ITERATE; j++)
         if(check_sub[i].left[j] != -1){ // 当たったときの自分のcount数が判明したとき
-          // flgをITERATEで管理したとき：block[i][index].flg[flg_cnt] = check_sub[i].left[j]; // blockにそのcount数を格納する
           block[i][index].flg[check_sub[i].left[j]] = 1; // 当たりだったcount値は1
-          // flg_cnt++;
         }
     }
   }
@@ -237,91 +192,102 @@ int check(int index){
 }
 
 /*dist_each_sub: 指定されたiterate, indexのbitをcに変換し、新たなindexの箇所に挿入する------------------*/
-void dist_each_sub(int iterate, int index, int bit, unsigned char c, unsigned char data[]){
+void dist_each_sub(int iterate, int index, int bit, unsigned char c, unsigned char from[]){
   int new_index = 0;
+  int data_count = 0;
   id_char[bit] = c;
-  new_index = hexa2num(id_char);
-  if(new_index <= 5555){
+  new_index = quad2num(id_char);
+  if(atari[new_index] == 0 && new_index <= 5555){
     for(int i=0; i<18; i++){
-      block[iterate][new_index].data[block[iterate][new_index].count][i] = data[i];
+      data_count = block[iterate][new_index].count;
+      block[iterate][new_index].data[data_count][i] = from[i];
     }
-    block[iterate][new_index].count++;
+    block[iterate][new_index].dist_plus++;
+  }else{
+    return;
   }
-  
+  return;
 }
 
 /*dist_each: 分配指せる箇所のiterate, index, とbitの桁を指定し、分配を行う--------------------------------*/
-void dist_each(int iterate, int index, int bit, unsigned char data[]){
-  num2hexa(index);
-  if(id_char[bit] == 'A'){
-    dist_each_sub(iterate, index, bit, 'C', data);
-    dist_each_sub(iterate, index, bit, 'G', data);
-    dist_each_sub(iterate, index, bit, 'T', data);
-  }else if(id_char[bit] == 'C'){
-    dist_each_sub(iterate, index, bit, 'A', data);
-    dist_each_sub(iterate, index, bit, 'G', data);
-    dist_each_sub(iterate, index, bit, 'T', data);
-  }else if(id_char[bit] == 'G'){
-    dist_each_sub(iterate, index, bit, 'A', data);
-    dist_each_sub(iterate, index, bit, 'C', data);
-    dist_each_sub(iterate, index, bit, 'T', data);
-  }else if(id_char[bit] == 'T'){
-    dist_each_sub(iterate, index, bit, 'A', data);
-    dist_each_sub(iterate, index, bit, 'C', data);
-    dist_each_sub(iterate, index, bit, 'G', data);
+void dist_each(int iterate, int index, unsigned char data[]){
+  num2quad(index);
+  for(int bit =0; bit<7; bit++){
+    if(id_char[bit] == 'A'){
+      dist_each_sub(iterate, index, bit, 'C', data);
+      dist_each_sub(iterate, index, bit, 'G', data);
+      dist_each_sub(iterate, index, bit, 'T', data);
+    }else if(id_char[bit] == 'C'){
+      dist_each_sub(iterate, index, bit, 'A', data);
+      dist_each_sub(iterate, index, bit, 'G', data);
+      dist_each_sub(iterate, index, bit, 'T', data);
+    }else if(id_char[bit] == 'G'){
+      dist_each_sub(iterate, index, bit, 'A', data);
+      dist_each_sub(iterate, index, bit, 'C', data);
+      dist_each_sub(iterate, index, bit, 'T', data);
+    }else if(id_char[bit] == 'T'){
+      dist_each_sub(iterate, index, bit, 'A', data);
+      dist_each_sub(iterate, index, bit, 'C', data);
+      dist_each_sub(iterate, index, bit, 'G', data);
+  } 
   }
+  return;
 }
 
 /*distribute_sub: 指定されたindexにおける余ったデータを分配する----------------------------------------------*/
 void distribute_sub(int index){
   // int flg_check = 0; // 探索中のcount値が当たりだったときに1
+  int data_count = 0;
   for(int i=0; i<ITERATE; i++){ // 各iterateを探索
-    for(int j=0; j<block[i][index].count; j++){ // 指定したindexの各count値が当たりであるか探索
-      /*flg_check = 0;
-      for(int k=0; k<ITERATE; k++){ // 各count値に対して, flgの内の1つと合致するか探索
-        if(j == block[i][index].flg[k]){
-          flg_check = 1; break;
-        }
-      }*/
+  data_count = block[i][index].count;
+    for(int j=0; j< data_count; j++){ // 指定したindexの各count値が当たりであるか探索
       if(block[i][index].flg[j] == 1) continue; // 当たりであればスルー
       else{ // 外れであれば分配する必要あり
-        for(int k=0; k<7; k++){ // データの分配
-          dist_each(i, index, k, block[i][index].data[j]);
-        }
+        dist_each(i, index, block[i][index].data[j]);
       }
     }
   }
+  return;
 }
 
 /*distribute: 分配を行う関数------------------------------------------------------------------------------*/
 void distribute(){
   int check_res = 0;
   for(int i=0; i<BLOCK_LEN; i++){
+    check_res = 0;
     check_res = check(i);
     if(check_res == 1) distribute_sub(i); // 当たりが含まれていれば、残りを分配する
   }
-  
+  return;
 }
 
+/*re_count: countを再集計*/
+void re_count(){
+  for(int i=0; i<ITERATE; i++){
+    for(int j=0; j<BLOCK_LEN; j++){
+      block[i][j].count = block[i][j].count + block[i][j].dist_plus;
+    }
+  }
+}
 
 /*Toblock_sub: ファイルから25文字文字を読み出し、インデックスの箇所に挿入する--------------------------------*/
 void Toblock_sub(int num, int kaisu, FILE *fp){
   unsigned char c1[7], c2[18];
-  unsigned char dis;
+  // unsigned char dis;
   int index = 0;
   int cum_count =0;
-  if(num != 0 && kaisu == 0) dis = fgetc(fp); /*改行を退避させる*/
+  if(num != 0 && kaisu == 0) fgetc(fp); /*改行を退避させる*/
   for(int i=0; i<25; i++){
     if(i<7) c1[i] = fgetc(fp);
     else    c2[i-7] = fgetc(fp);
   }
-  index = hexa2num(c1); // インデックスを計算
+  index = quad2num(c1); // インデックスを計算
   cum_count = block[num][index].count;
   for(int i=0; i<18; i++){ // 指定箇所にデータの書込み
     block[num][index].data[cum_count][i] = c2[i];
   }
   block[num][index].count++;
-
+  return;
 }
 
 /*Toblock: ファイルから全ての文字列を読み出し、インデックスの箇所に挿入する-----------------------------------*/
@@ -331,6 +297,7 @@ void Toblock(FILE *fp){
       Toblock_sub(i, j, fp);
     }
   }
+  return;
 }
 
 /*md_sub: 4つの中から最大のものを取り出す------------------------------------------------------------------*/
@@ -406,6 +373,7 @@ int dec(){
   init_block(); //blockの初期化
   Toblock(sfp); // 文字列をblockに格納
   distribute(); // 当たりがあるか判定し、当たりがあった場所の余りのデータは分配する
+  re_count();
   for(int i=0; i<BLOCK_LEN; i++){ // 分配後、前のcheckで外れ判定を受けた箇所に、再度checkを行う
     if(atari[i] == 0)
       check(i);
@@ -429,17 +397,23 @@ int dec(){
   printf("all_count: %d\n", all_count);
   int count_max=0;
   int zero_cnt = 0;
+  int zero_total = 0;
+  int block_cnt =0;
   for(int i=0; i<BLOCK_LEN; i++){
-    printf("%d: ", i);
+    //printf("%d: ", i);
     zero_cnt = 0;
+    block_cnt = 0;
     for(int j=0; j<ITERATE; j++){
-      printf("%d ", block[j][i].count);
+      //printf("%d ", block[j][i].count);
+      block_cnt += block[j][i].count;
       if(block[j][i].count == 0) zero_cnt++;
     }
-    if(zero_cnt == ITERATE) count_max++; 
-    printf("\n");
+    if(zero_cnt == ITERATE) zero_total++;
+    if(block_cnt > count_max) count_max = block_cnt; 
+    //printf("\n");
   }
   printf("count_max: %d\n", count_max);
+  printf("zero_total: %d\n", zero_total);
 
   res = '\n';
   fputc(res, dfp);
