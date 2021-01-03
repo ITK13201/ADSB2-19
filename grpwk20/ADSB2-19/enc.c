589070d1be8c5b9ab19a4433aa6a689b5ea43a26
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2021-01-03 19:30:56 +0900

    formatted by vscode

--- grpwk20/ADSB2-19/enc.c
+++ grpwk20/ADSB2-19/enc.c
@@ -4,36 +4,42 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("avx2")
 #pragma GCC optimize("unroll-loops")
-#define rep(i,n) for(register int (i)=0;(i)<(n);(i)++)
+#define rep(i, n) for (register int(i) = 0; (i) < (n); (i)++)
 
-int enc(){
-  FILE *ofp;
-  if((ofp = fopen(ORGDATA, "r")) ==NULL){
-    fprintf(stderr, "cannot open %s\n", ORGDATA);
-    exit(1);
-  }
-  FILE *efp;
-  if((efp = fopen(ENCDATA, "w")) ==NULL){
-    fprintf(stderr, "cannot open %s\n", ENCDATA);
-    exit(1);
-  }
-  setvbuf(ofp,NULL,_IOFBF,1048576);
-  setvbuf(efp,NULL,_IOFBF,1048576);
-  char in[200000],out[200000];
-  fread(in,sizeof(char),200000,ofp);
-  rep(i,99999){
-    out[(i<<1)]=(in[(i<<1)]=='1'?'G':'A');
-    out[(i<<1)+1]=(in[(i<<1)+1]=='1'?'C':'T');
-  }
-  char c1=in[199998],c2=in[199999];
-  int pt=(c1-'0')*2+(c2-'0');
-  out[199998]=(pt==0?'A':pt==1?'T':pt==2?'G':'C');
-  out[199999]='\n';
-  fwrite(out,sizeof(char),200000,efp);
-  fclose(ofp),fclose(efp);
-  return(0);
+int enc()
+{
+    FILE *ofp;
+    if ((ofp = fopen(ORGDATA, "r")) == NULL)
+    {
+        fprintf(stderr, "cannot open %s\n", ORGDATA);
+        exit(1);
+    }
+    FILE *efp;
+    if ((efp = fopen(ENCDATA, "w")) == NULL)
+    {
+        fprintf(stderr, "cannot open %s\n", ENCDATA);
+        exit(1);
+    }
+    setvbuf(ofp, NULL, _IOFBF, 1048576);
+    setvbuf(efp, NULL, _IOFBF, 1048576);
+    char in[200000], out[200000];
+    fread(in, sizeof(char), 200000, ofp);
+    rep(i, 99999)
+    {
+        out[(i << 1)] = (in[(i << 1)] == '1' ? 'G' : 'A');
+        out[(i << 1) + 1] = (in[(i << 1) + 1] == '1' ? 'C' : 'T');
+    }
+    char c1 = in[199998], c2 = in[199999];
+    int pt = (c1 - '0') * 2 + (c2 - '0');
+    out[199998] = (pt == 0 ? 'A' : pt == 1 ? 'T' : pt == 2 ? 'G' : 'C');
+    out[199999] = '\n';
+    fwrite(out, sizeof(char), 200000, efp);
+    fclose(ofp), fclose(efp);
+    return (0);
 }
-int main(){
-  enc();
-  return(0);
+
+int main()
+{
+    enc();
+    return (0);
 }
